d93a59ef8f521b0d377d976013cefaee71316a4a
Jean <acalto2@gmail.com>
2023-09-30 22:33:59 +0200

    HomeScreen, ButtonWidget

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:todo_x/colors/app_colors.dart';
+import 'package:todo_x/widgets/button_widget.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.maxFinite,
+        height: double.maxFinite,
+        padding: const EdgeInsets.only(
+          left: 20,
+          right: 20,
+        ),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage('assets/welcome.jpg'),
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            RichText(
+              text: const TextSpan(
+                text: 'Hello',
+                style: TextStyle(
+                  color: AppColors.mainColor,
+                  fontSize: 60,
+                  fontWeight: FontWeight.bold,
+                ),
+                children: [
+                  TextSpan(
+                    text: '\nstart your beautiful day',
+                    style: TextStyle(
+                      color: AppColors.smallTextColor,
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 3,
+            ),
+            const ButtonWidget(
+              backgroundcolor: AppColors.mainColor,
+              text: 'Add Task',
+              textColor: Colors.white,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const ButtonWidget(
+              backgroundcolor: Colors.white,
+              text: 'View All',
+              textColor: AppColors.smallTextColor,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/button_widget.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+class ButtonWidget extends StatelessWidget {
+  final Color backgroundcolor;
+  final String text;
+  final Color textColor;
+
+  const ButtonWidget({
+    Key? key,
+    required this.backgroundcolor,
+    required this.text,
+    required this.textColor,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.maxFinite,
+      height: MediaQuery.of(context).size.height / 14,
+      decoration: BoxDecoration(
+        color: backgroundcolor,
+        borderRadius: BorderRadius.circular(40),
+      ),
+      child: Center(
+        child: Text(
+          text,
+          style: TextStyle(
+            fontSize: 24,
+            color: textColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
